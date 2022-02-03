6a3467ba1521c337f27120ba59237ba47a2b3e7d
pietroline <pietrobarberi@hotmail.it>
2022-02-03 15:52:07 +0100

    snack 2 completato

---
+++ snack2/js/main.js
@@ -0,0 +1,48 @@
+//random(min, max) ritorna un numero random tra il min e max inclusi 
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min); 
+}
+
+function creaNuovoArray(numeroSquadre, elencoSquadre){
+
+    const nuovoArray = [];
+    for(let i=0; i<numeroSquadre; i++){
+        let {nome, falli_subiti} = elencoSquadre[i];
+        nuovoArray[i] = [nome, falli_subiti];
+    }
+
+    return nuovoArray;
+
+}
+
+const NUMERO_SQUADRE = 10;
+
+
+
+// -----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+// Creo un array di oggetti utilizzando il ciclo for per non inserire manualmete ogni squadra, ripetendo del codice
+const mySquadre = [];
+for(let i=0; i<NUMERO_SQUADRE; i++){
+    
+    mySquadre[i] = {
+        "nome": `squadra-${i+1}`,
+        "punti fatti": 0,
+        "falli_subiti": 0,
+    };
+
+    mySquadre[i]["punti fatti"] = random(100, 1000);
+    mySquadre[i].falli_subiti = random(0, 10);
+
+}
+console.log(mySquadre);
+
+//invoco creaNuovoArray(numeroSquadre, elencoSquadre) al fine di creare il nuovo array secondo specifiche di progetto
+const nuovoArray = creaNuovoArray(NUMERO_SQUADRE, mySquadre);
+
+// eseguo la stampa di tutti i dati richiesti
+for(let i=0; i<nuovoArray.length; i++){
+    console.log(`${nuovoArray[i][0]} ha subìto ${nuovoArray[i][1]} falli`);
+}
